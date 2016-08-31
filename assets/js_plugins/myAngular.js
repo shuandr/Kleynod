69d3c17798165ecf964c892b212f4aca269b6550
shuandr <shuandr@gmail.com>
2016-08-31 15:09:24 +0300

    baget width selector

--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -24,8 +24,14 @@ app.config(function($interpolateProvider) {
 app.controller('mouldCtrl', function($scope, $http) {
     $scope.mouldProducent = ["","Сербія", "Іспанія", "Польща", "Україна"];
     $scope.mouldMaterial = ["", "дерево", "пластик", "метал"];
-    $scope.mouldWidths = ['', 14,15,17,20,22,24,29,32,42,49,63,70,72,87,90,92];
-    // $scope.mouldWidths = ['', '<25 mm','25-50 mm', '50-90 mm', '>90 mm'];
+    // $scope.mouldWidths = ['', 14,15,17,20,22,24,29,32,42,49,63,70,72,87,90,92];
+    $scope.mouldWidths = [
+    {value: '1'|'2'|'3'|'4', option:'всі'}, 
+    {value: '1', option:'<25 mm'}, 
+    {value: '2', option:'25-50 mm'}, 
+    {value: '3', option:'50-90 mm'}, 
+    {value: '4', option:'>90 mm'}
+    ];
     // $scope.mouldWidths = ['', '<25', '>25' & '<50', '>50' & '<90', '>90'];
     $scope.selectedProducent = $scope.mouldProducent[0];
     $scope.selectedMaterial = $scope.mouldMaterial[0];
@@ -63,9 +69,20 @@ app.controller('mouldCtrl', function($scope, $http) {
         $scope.allMoulds = $scope.allMoulds.concat($scope.espanaMoulds);
         /*$scope.mouldWidths = $scope.mouldWidths.concat($scope.espanaMoulds.width);
         console.log ($scope.serbMoulds.width); */
+        angular.forEach($scope.allMoulds, function(obj) {
+            if(obj.width <=25){
+                obj.widthRange = '1';
+            } else if(obj.width >25 && obj.width <=50 )  {
+                obj.widthRange = '2';
+            } else if (obj.width >50 && obj.width <=90 )  {
+                obj.widthRange = '3';
+            } else {
+                obj.widthRange = '4';
+            } 
 
+        });
     });
-
+// debugger;
     
 
 
