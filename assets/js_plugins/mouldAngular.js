1953cf7626a0b148e6cb04b70c37ef29059da30a
shuandr <shuandr@gmail.com>
2022-04-01 13:40:28 +0300

    v. 3.2
    added EN version

--- assets/js_plugins/mouldAngular.js
+++ assets/js_plugins/mouldAngular.js
@@ -7,14 +7,35 @@ app.config(function($interpolateProvider) {
 
 app.controller('mouldCtrl', function($scope, $http) {
 
-    $http.get("assets/data/data.json").then(function(response) {
-        $scope.data = response.data;
+    $http.get("assets/data/UA-data.json").then(function(response) {
+        $scope.UAdata = response.data;
+        $scope.data = $scope.UAdata;
+        $scope.mouldProducent = $scope.data.mouldCat.mouldProducent;
+        $scope.mouldMaterial = $scope.data.mouldCat.mouldMaterial;
+    });
+    $http.get("assets/data/EN-data.json").then(function(response) {
+        $scope.ENdata = response.data;
 
     });
 
+    var euroExchange = 34.5;
 
-    $scope.mouldProducent = ["", "Іспанія", "Італія", "США", "Сербія", "Польща", "Україна"];
-    $scope.mouldMaterial = ["", "дерево", "МДФ", "пластик"];
+    $scope.setLang = "en";
+
+    $scope.changeLang = function() {
+        if ($scope.setLang == "en") {
+
+            $scope.setLang = "ua";
+            $scope.data = $scope.ENdata;
+            $scope.mouldProducent = $scope.data.mouldCat.mouldProducent;
+        $scope.mouldMaterial = $scope.data.mouldCat.mouldMaterial;
+
+        } else {
+            $scope.data = $scope.UAdata;
+            $scope.setLang = "en";
+        }
+    };
+
     $scope.mouldWidths = [{
         value: '',
         option: 'всі'
@@ -32,7 +53,6 @@ app.controller('mouldCtrl', function($scope, $http) {
         option: '>90 mm'
     }];
 
-    var euroExchange = 34.5;
     var assignWidthRange = function(obj) {
         if (obj.width <= 25) {
             obj.widthRange = '1';
@@ -55,32 +75,32 @@ app.controller('mouldCtrl', function($scope, $http) {
         $scope.allMouldsCatalog = response.data;
 
         $scope.allMouldsCatalog.garcia.forEach(function(obj) {
-            obj.producent = 'Іспанія';
-            obj.material = "дерево";
+            obj.producent = 'es';
+            obj.material = "w";
             obj.price *= euroExchange;
         });
         $scope.allMouldsCatalog.injac.forEach(function(obj) {
-            obj.producent = 'Сербія';
-            obj.material = "дерево";
+            obj.producent = 'sr';
+            obj.material = "w";
             obj.price *= euroExchange;
 
         });
         $scope.allMouldsCatalog.framerica.forEach(function(obj) {
-            obj.producent = 'США';
-            obj.material = "МДФ";
+            obj.producent = 'us';
+            obj.material = "m";
         });
         $scope.allMouldsCatalog.esel.forEach(function(obj) {
-            obj.producent = 'Італія';
-            obj.material = "дерево";
+            obj.producent = 'it';
+            obj.material = "w";
             obj.price *= euroExchange;
         });
         $scope.allMouldsCatalog.werrama.forEach(function(obj) {
-            obj.producent = 'Польща';
-            obj.material = "дерево";
+            obj.producent = 'pl';
+            obj.material = "w";
         });
         $scope.allMouldsCatalog.clever.forEach(function(obj) {
-            obj.producent = 'Україна';
-            obj.material = "пластик";
+            obj.producent = 'ua';
+            obj.material = "p";
             obj.price *= euroExchange;
         });
 
--- assets/js_plugins/myAngular.js
+++ assets/js_plugins/myAngular.js
@@ -16,10 +16,29 @@ app.controller('kleynodCtrl', function($scope, $http) {
 
 
 
-    $http.get("assets/data/data.json").then(function(response) {
-        $scope.data = response.data;
+    $http.get("assets/data/UA-data.json").then(function(response) {
+        $scope.UAdata = response.data;
+        $scope.data = $scope.UAdata;
 
     });
+    $http.get("assets/data/EN-data.json").then(function(response) {
+        $scope.ENdata = response.data;
+
+    });
+
+    $scope.setLang = "en";
+
+    $scope.changeLang = function() {
+        if ($scope.setLang == "en") {
+           
+            $scope.data = $scope.ENdata;
+            $scope.setLang = "ua";
+
+        } else {
+            $scope.data = $scope.UAdata;
+            $scope.setLang = "en";
+        }
+    };
 
 
 
